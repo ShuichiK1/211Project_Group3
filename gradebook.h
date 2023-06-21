421d3c2b6e03ab866343bb8f39c6779406715d9a
james-mccaffrey <136614316+james-mccaffrey@users.noreply.github.com>
2023-06-21 17:27:41 -0400

    Update gradebook.h

--- gradebook.h
+++ gradebook.h
@@ -1,1 +1,42 @@
+#ifndef UNTITLED8_GRADEBOOK_H
+#define UNTITLED8_GRADEBOOK_H
 
+#include "iostream"
+#include "vector"
+#include <fstream>
+#include <sstream>
+
+class GradeBook{
+private:
+    std::vector<std::vector<std::string>> inputVec;
+    std::vector<std::vector<double>> grades;
+
+    std::vector<std::string> categories;
+    std::vector<double> categoryGrades;
+    std::vector<std::string> categoryLetterGrades;
+
+    std::vector<std::vector<std::string>> letterGradesArr;
+
+    double courseGrade;
+    std::string courseLetGrade;
+
+    std::string fname;
+    std::string courseName;
+    
+public:
+    GradeBook();
+    GradeBook(std::vector<std::vector<std::string>> vec, std::vector<std::string> categories, std::string courseName, std::string fname);
+
+    std::string getLetterGrade(double grade);
+
+    void calcGrades();
+
+    bool outputIndividual( std::string target);
+    bool outputCat(std::string target);
+
+    void outputAll();
+    void outputCatTot();
+    void outputOver();
+};
+
+#endif //UNTITLED8_GRADEBOOK_H
