b0a126d42927b9a168612b7d94779b416f336838
ShuichiK1 <78115987+ShuichiK1@users.noreply.github.com>
2023-06-26 07:58:19 -0400

    Update Pseudocode.cpp

---
+++ Pseudocode.cpp
@@ -0,0 +1,28 @@
+--------------- | Main | --------------------
+  
+Get file name and read file
+[UI] that allows user to choose function
+Tokenize file and push to vectors
+Create gradebook object from vectors
+
+[UI] :
+    - Different options can be chosen by inputting 
+      corresponding value into terminal
+    - Continuously repeats until input received
+    - Used to satisfy project requirements (display category
+      grades, overall, etc.)
+    
+---------------------------------------------
+
+----------- | Gradebook.cpp | ---------------
+
+Constructor that stores tokenized argument into seperate vectors
+Method that outputs letter grade depending on numerical grade
+Method that prints output to terminal
+  
+[Calculate grades] :
+    - Method that pulls from vectors depending on process
+    - Accounts for point system to calculate averages
+
+  
+  
